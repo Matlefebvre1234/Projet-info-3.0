430ba5cf7be21eae4c991be1abdcc011c2d03e38
Mathieu <mat_lefebvre12@hotmail.com>
2020-02-13 12:34:23 -0500

    archer site

--- projet-info/Assets/scripts/Mathieu/ArcherTesting.cs
+++ projet-info/Assets/scripts/Mathieu/ArcherTesting.cs
@@ -25,22 +25,29 @@ public class ArcherTesting : MonoBehaviour
         {
             pathfinding.limiteDistance = 0;
             positionSouris = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        }
-     
             pathfinding.getGrid().GetXY(positionSouris, out int x2, out int y2);
             pathfinding.getGrid().GetXY(transform.position, out int x1, out int y1);
 
-            chemin = pathfinding.FindPath(x1,y1, x2, y2);
+            chemin = pathfinding.FindPath(x1, y1, x2, y2);
+            // chemin = pathfinding.FindPath(0, 0, x2, y2);
+            SuivreChemin();
+        }
+       
+           
+            SuivreChemin();
+        
 
-            if (chemin != null)
+
+
+        if (chemin != null)
             {
-                // for (int i = 0; i < chemin.Count - 1; i++)
-                //{
-                //    Debug.Log(chemin[i].x + " , " + chemin[i].y + " G: " + chemin[i].Gcost + " h: " + chemin[i].Hcost + " F: " + chemin[i].Gcost);
-                //    Debug.DrawLine(new Vector3(chemin[i].x, chemin[i].y) * 0.5f + Vector3.one * 0.25f + new Vector3(4, 0.5f), new Vector3(chemin[i + 1].x, chemin[i + 1].y) * 0.5f + Vector3.one * 0.25f + new Vector3(4, 0.5f), Color.green, 100f);
-                //}
+                 for (int i = 0; i < chemin.Count - 1; i++)
+                {
 
-                SuivreChemin();
+                    Debug.DrawLine(new Vector3(chemin[i].x, chemin[i].y) * 0.5f + Vector3.one * 0.25f + new Vector3(4, 0.5f), new Vector3(chemin[i + 1].x, chemin[i + 1].y) * 0.5f + Vector3.one * 0.25f + new Vector3(4, 0.5f), Color.green, 100f);
+                }
+
+                
 
 
 
@@ -50,13 +57,15 @@ public class ArcherTesting : MonoBehaviour
 
     }
 
-    private void SuivreChemin()
+   private void SuivreChemin()
     {
         if (chemin != null)
         {
-            pathfinding.getGrid().GetWorldXY(new Vector2(chemin[index].x, chemin[index].y), out int x, out int y);
+            Debug.Log (chemin[index].x + " " + chemin[index].y);
+            pathfinding.getGrid().GetWorldXY(new Vector2(chemin[index].x, chemin[index].y), out float x, out float y);
             Vector2 targetPosition = new Vector2(x, y);
-            if (Vector2.Distance(transform.position, targetPosition) > 0.01f)
+            Debug.Log(targetPosition);
+            if (Vector2.Distance(transform.position, targetPosition) > 0.0001f)
             {
 
                 transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed);
@@ -67,7 +76,7 @@ public class ArcherTesting : MonoBehaviour
                 if (index >= chemin.Count)
                 {
                     chemin = null;
-
+                    index = 0;
                 }
             }
 
@@ -76,5 +85,5 @@ public class ArcherTesting : MonoBehaviour
         }
         
 
-    }
+    } 
 }
