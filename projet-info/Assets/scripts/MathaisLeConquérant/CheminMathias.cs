2e6339be0d49c1ee931a32d85d4e5a940249d031
MG <mathgames@hotmail.ca>
2020-02-12 21:02:26 -0500

    Mathias=bigpp
    g comencé

---
+++ projet-info/Assets/scripts/MathaisLeConquérant/CheminMathias.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheminMathias : MonoBehaviour
+{
+    private Grid<CheminMathias> grille;
+    private int x;
+    private int y;
+
+    public int g;
+    public int h;
+    public int f;
+
+    public CheminMathias casePrecedente;
+
+    public CheminMathias(Grid<CheminMathias> n_grille, int n_x, int n_y)
+    {
+        grille = n_grille;
+        x = n_x;
+        y = n_y;
+    }
+
+    public override string ToString()
+    {
+        return x + ", " + y;
+    }
+}
