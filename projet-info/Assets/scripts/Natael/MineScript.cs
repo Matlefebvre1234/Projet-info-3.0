d961fb82d78039ef94ad0e2e47f196f651efdc94
Natael <nataellaroche@hotmail.com>
2020-02-18 14:25:20 -0500

    début animation

--- projet-info/Assets/scripts/Natael/MineScript.cs
+++ projet-info/Assets/scripts/Natael/MineScript.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class MineScript : MonoBehaviour
 {
+    public float rangeExplosion = 0.5f;
+
+    private float distance;
+
+    GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        distance = Vector2.Distance(transform.position, player.transform.position);
+       if(distance < rangeExplosion)
+        {
+            //Explosion et dégats
+        }
     }
 }
