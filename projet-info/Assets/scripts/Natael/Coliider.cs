7ad857ddf8c384bbdfc89710a0b34d86e6ebf29b
Natael <nataellaroche@hotmail.com>
2020-03-12 16:19:41 -0400

    niveau 1+2+3

---
+++ projet-info/Assets/scripts/Natael/Coliider.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coliider : MonoBehaviour
+{
+    public GameObject camera1;
+    public GameObject camera2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player" && camera1.activeSelf == false)
+        {
+            camera2.SetActive(false);
+            camera1.SetActive(true);
+        }
+        else
+        {
+            camera1.SetActive(false);
+            camera2.SetActive(true);            
+        }
+    }
+
+    //private void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    if (collision.gameObject.tag == "Player" && camera1.activeSelf == false)
+    //    {
+    //        camera2.SetActive(false);
+    //        camera1.SetActive(true);
+    //    }
+    //    else
+    //    {
+    //        camera1.SetActive(false);
+    //        camera2.SetActive(true);
+    //    }
+    //}
+}
