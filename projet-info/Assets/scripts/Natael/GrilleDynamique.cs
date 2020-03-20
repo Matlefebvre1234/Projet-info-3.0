d3f0dfee42f39c73d13518767e8bd13e5079d64f
Natael <nataellaroche@hotmail.com>
2020-03-20 10:46:57 -0400

    colonne réparer

---
+++ projet-info/Assets/scripts/Natael/GrilleDynamique.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrilleDynamique : MonoBehaviour
+{
+    static GrilleNatael grid;
+    public int hauteur = 14;
+    public int largeur = 22;
+    public float dimCell = 0.5f;
+    public static Vector3 origine = new Vector3(8, 1);
+    // Start is called before the first frame update
+    void Start()
+    {
+        grid = new GrilleNatael(largeur, hauteur, dimCell, origine);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GrilleNatael getGrid()
+    {
+        if (grid == null)
+        {
+            grid = new GrilleNatael(largeur, hauteur, dimCell, origine);
+        }
+
+        return grid;
+
+    }
+    public float getDimCell()
+    {
+        return dimCell;
+    }
+}
