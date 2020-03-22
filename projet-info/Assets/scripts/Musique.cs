0d8d4124edabd57b75a09cf8e407883c29c27e35
Mathias <mathgames@hotmail.ca>
2020-03-22 11:18:09 -0400

    Ajout musique de fond

---
+++ projet-info/Assets/scripts/Musique.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Musique : MonoBehaviour
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+}
