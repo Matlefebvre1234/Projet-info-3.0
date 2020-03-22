95979a2d6234c7d8b6fdf49e4faa3c8bd3eb9134
Mathias <mathgames@hotmail.ca>
2020-03-22 14:57:39 -0400

    Machine

---
+++ projet-info/Assets/MachineKitSoin.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineKitSoin : MonoBehaviour
+{
+    public GameObject kitSoin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void OnEnterTrigger2D(Collider2D collider)
+    {
+        Debug.Log("allo");
+        if (collider.gameObject.tag == "Player")
+        {
+            Instantiate(kitSoin, (Vector2)transform.position - new Vector2(0.25f, 0.25f), Quaternion.identity);
+            Debug.Log("allo");
+        }
+    }
+}
