6a63ffa18c53a9c7c8f802bbc11ebb48d6c1c59c
Natael <nataellaroche@hotmail.com>
2020-05-09 11:31:21 -0400

    Skin qui fonctionne !

--- projet-info/Assets/Magasin.cs
+++ projet-info/Assets/Magasin.cs
@@ -11,13 +11,13 @@ public class Magasin : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        PlayerPrefs.SetInt("Skin", 0);
-        PlayerPrefs.SetInt("Skin_1", 0);
-        PlayerPrefs.SetInt("Skin_2", 0);
-        PlayerPrefs.SetInt("Skin_3", 0);
-        PlayerPrefs.SetInt("Skin_4", 0);
-        PlayerPrefs.SetInt("Niveau", 1);
-        PlayerPrefs.SetInt("Sort Ultime", 0);
+        //PlayerPrefs.SetInt("Skin", 0);
+        //PlayerPrefs.SetInt("Skin_1", 0);
+        //PlayerPrefs.SetInt("Skin_2", 0);
+        //PlayerPrefs.SetInt("Skin_3", 0);
+        //PlayerPrefs.SetInt("Skin_4", 0);
+        //PlayerPrefs.SetInt("Niveau", 1);
+        //PlayerPrefs.SetInt("Sort Ultime", 0);
     }
 
     public void changerMenuInventaire()
@@ -46,8 +46,8 @@ public class Magasin : MonoBehaviour
             if (PlayerPrefs.GetInt("Argent Joueur Skin") >= 1000 && PlayerPrefs.GetInt("Skin_1") != 1)
             {
                 //Rouge
-                Debug.Log("Skin_1");
                 PlayerPrefs.SetInt("Skin_1", 1);
+                PlayerPrefs.SetInt("Skin choisit", 1);
                 PlayerPrefs.SetInt("Argent Joueur Skin", PlayerPrefs.GetInt("Argent Joueur Skin") - 1000);
             }
         }
@@ -57,6 +57,7 @@ public class Magasin : MonoBehaviour
             {
                 //Turquoise
                 PlayerPrefs.SetInt("Skin_2", 1);
+                PlayerPrefs.SetInt("Skin choisit", 2);
                 PlayerPrefs.SetInt("Argent Joueur Skin", PlayerPrefs.GetInt("Argent Joueur Skin") - 1000);
             }
         }
@@ -66,6 +67,7 @@ public class Magasin : MonoBehaviour
             if (PlayerPrefs.GetInt("Argent Joueur Skin") >= 1000 && PlayerPrefs.GetInt("Skin_3") !=1)
             {
                 PlayerPrefs.SetInt("Skin_3", 1);
+                PlayerPrefs.SetInt("Skin choisit", 3);
                 PlayerPrefs.SetInt("Argent Joueur Skin", PlayerPrefs.GetInt("Argent Joueur Skin") - 1000);
             }
         }
@@ -75,18 +77,19 @@ public class Magasin : MonoBehaviour
             if (PlayerPrefs.GetInt("Argent Joueur Skin") >= 1000 && PlayerPrefs.GetInt("Skin_4") != 1)
             {
                 PlayerPrefs.SetInt("Skin_4", 1);
+                PlayerPrefs.SetInt("Skin choisit", 4);
                 PlayerPrefs.SetInt("Argent Joueur Skin", PlayerPrefs.GetInt("Argent Joueur Skin") - 1000);
             }
         }
         else if (button.name == "Niveau")
         {
-            if (PlayerPrefs.GetInt("Argent Joueur Skin") >= 5000)
+            if (PlayerPrefs.GetInt("Argent Joueur Skin") >= 5000 && PlayerPrefs.GetInt("Niveau") <=6)
             {
                 PlayerPrefs.SetInt("Niveau", PlayerPrefs.GetInt("Niveau") + 1);
                 PlayerPrefs.SetInt("Argent Joueur Skin", PlayerPrefs.GetInt("Argent Joueur Skin") - 5000);
             }
         }
-        else if (button.name == "SortUltime")
+        else if (button.name == "SortUltime" && PlayerPrefs.GetInt("Sort Ultime") != 1)
         {
             if (PlayerPrefs.GetInt("Argent Joueur Skin") >= 1000000)
             {
