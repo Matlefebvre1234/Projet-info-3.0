014d05297f498c0d3618291d833ffa976f0c851f
Natael <nataellaroche@hotmail.com>
2020-05-14 19:50:58 -0400

    Bug son

--- projet-info/Assets/scripts/EffetSonores.cs
+++ projet-info/Assets/scripts/EffetSonores.cs
@@ -1,27 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EffetSonores : MonoBehaviour
 {
     public AudioSource audioSrc;
+    public Slider sliderEffetSonore;
     public float effetsVol = 1f;
     
 
     private void Start()
     {
-        if (!audioSrc.isPlaying.Equals(true))
+        if (this.transform.gameObject.tag == "Musique")
         {
+            sliderEffetSonore.value = (PlayerPrefs.GetFloat("effetsSonores"));
+        }
+        
+        if (!audioSrc.isPlaying.Equals(true))
+        {            
             effetsVol = audioSrc.volume;
+            effetsVol = PlayerPrefs.GetFloat("effetsSonores");
+        }
+        else
+        {
             effetsVol = PlayerPrefs.GetFloat("effetsSonores");
         }
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {        
         audioSrc.volume = effetsVol;
         PlayerPrefs.SetFloat("effetsSonores", effetsVol);
+        Debug.Log("Effets sonores volume = " + PlayerPrefs.GetFloat("effetsSonores"));
     }
 
     public void SetVolume(float vol)
--- projet-info/Assets/scripts/Musique.cs
+++ projet-info/Assets/scripts/Musique.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Musique : MonoBehaviour
 {
     private static Musique instance = null;
+
     public static Musique Instance
     {
         get
@@ -14,21 +16,36 @@ public class Musique : MonoBehaviour
     }
 
     public AudioSource audioSrc;
+    public Slider sliderMusique;
+
     private float musiqueVol = 1f;
 
     private void Start()
     {
+        if(this.transform.gameObject.tag == "Musique")
+        {
+            sliderMusique.value = PlayerPrefs.GetFloat("Musique");
+        }
+
         if (!audioSrc.isPlaying.Equals(true))
         {
+            
             musiqueVol = audioSrc.volume;
             musiqueVol = PlayerPrefs.GetFloat("Musique");
         }
+        else
+        {
+            musiqueVol = PlayerPrefs.GetFloat("Musique");
+        }
+
     }
 
     private void Update()
     {
         audioSrc.volume = musiqueVol;
         PlayerPrefs.SetFloat("Musique", musiqueVol);
+        Debug.Log("musique volume = " + PlayerPrefs.GetFloat("Musique"));
+
     }
 
     public void SetVolume(float vol)
@@ -38,18 +55,21 @@ public class Musique : MonoBehaviour
 
     void Awake()
     {
-        Debug.Log(instance);
-        if (instance != null)
+        if (!audioSrc.isPlaying.Equals(true))
         {
-            Destroy(this.gameObject);
-            return;
-        }
-        else
-        {
-            instance = this;
-        }
+            Debug.Log(instance);
+            if (instance != null)
+            {
+                Destroy(this.gameObject);
+                return;
+            }
+            else
+            {
+                instance = this;
+            }
 
 
-        DontDestroyOnLoad(transform.gameObject);
+            DontDestroyOnLoad(transform.gameObject);
+        }
     }
 }
